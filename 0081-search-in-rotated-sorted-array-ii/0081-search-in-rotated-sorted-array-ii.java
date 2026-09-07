a9662bb96f7f5398b8fb7909f952e20e4489ec6e
Sai prasad Padhy <saiprasadpadhy2006@gmail.com>
2026-09-07 20:02:35 +0530

    Time: 0 ms (100%), Space: 44.9 MB (81.91%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean search(int[] arr, int k) {
+        int l=0,h=arr.length-1;
+         while(l<=h){
+            int mid =(l+h)/2;
+            if(arr[mid]==k)return true;
+            if(arr[l]==arr[mid] && arr[mid]==arr[h]){
+                l++;h--;
+                continue;
+            }
+            if(arr[l]<=arr[mid]){
+                if(arr[l]<=k && k<=arr[mid]){
+                    h=mid-1;
+                }else{
+                    l=mid+1;
+                }
+            }else{
+                if(arr[mid]<=k && k<=arr[h]){
+                    l=mid+1;
+                }else{
+                    h=mid-1;
+                }
+            }
+         }return false;
+    }
+}
